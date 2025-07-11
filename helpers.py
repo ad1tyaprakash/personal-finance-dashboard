cdaaf9134eaff93be05800d42cac69728c318385
ad1tyaprakash <aditya.prakash2929@gmail.com>
2025-07-12 01:55:38 +0530

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,14 +1,14 @@
 from functools import wraps
 from flask import redirect, session
 import sqlite3
+import yfinance as yf
 
-# Connect to SQLite database
+
 def get_db_connection():
     conn = sqlite3.connect("finance.db")
     conn.row_factory = sqlite3.Row
     return conn
 
-# Decorator to protect routes (like /dashboard)
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -17,9 +17,15 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
-# Optional: Lookup user info by ID
 def get_user_by_id(user_id):
     conn = get_db_connection()
     user = conn.execute("SELECT * FROM users WHERE id = ?", (user_id,)).fetchone()
     conn.close()
     return user
+
+def get_current_price(symbol):
+    try:
+        stock = yf.Ticker(symbol)
+        return round(stock.info["regularMarketPrice"], 2)
+    except Exception:
+        return None
